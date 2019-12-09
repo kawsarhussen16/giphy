61f68337f0054fdfc31f71ea97be21f9ccd38c60
Kawsar Hussen <kawsarhussen@gmail.com>
2019-12-09 16:43:51 -0500

    SearchPage Implemented

---
+++ giphy/src/components/searchBar.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import styled from 'styled-components';
+import axios from 'axios';
+
+class SearchPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            option1: '',
+            option2: '',
+            keyword: '',
+            url: '',
+            gifs: "gifs",
+            data: {}
+        }
+    }
+    searchGifs = e => {
+        e.preventDefault();
+        this.setState({ gifs: "gifs", option1: 'gifs', option2: '' });
+        if (this.state.keyword.length > 0) {
+            this.fetchData();
+        }
+    }
+    searchStickers = e => {
+        e.preventDefault();
+        this.setState({ gifs: "stickers", option1: '', option2: 'stickers', })
+        if (this.state.keyword.length > 0) {
+            this.fetchData();
+        }
+    }
+    handleInput = e => {
+        e.preventDefault();
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    fetchData = async () => {
+        let APIKEY = "NByeny2ItNCWUfNnnyz9cIrI2vz65Bfd";
+        let url = `https://api.giphy.com/v1/${this.state.gifs}/search?api_key=${APIKEY}&limit=1&q=`;
+        let qry = url.concat(this.state.keyword)
+        let data = await axios.get(qry)
+        this.setState({ data: data.data.data })
+        console.log(this.state.data)
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBox>
+                    <Input onkeydown={this.fetchData} onChange={this.handleInput} value={this.state.keyword} name='keyword' placeholder='search' />
+                    <FontAwesomeIcon onClick={this.fetchData} className='SearchIcon' icon={faSearch} />
+                </SearchBox>
+                <OptionBox>
+                    <Button className={` ${this.state.option1}`} onClick={this.searchGifs} >GIFs</Button>
+                    <Button className={` ${this.state.option2}`} onClick={this.searchStickers} >Stickers</Button>
+
+                </OptionBox>
+
+            </div >
+        );
+
+    }
+
+}
+
+
+const SearchBox = styled.div`
+    margin: 10px;
+    text-align: center;
+    display: flex;
+    flex-wrap: wrap;
+    .SearchIcon{
+        padding: 0.5rem;
+        margin: 0.5rem;
+        cursor: pointer;
+    }
+
+`
+const Input = styled.input`
+    width: 70%;
+    padding: 0.5rem;
+    margin: 0.5rem;
+    border-radius: 5px;
+`
+
+const OptionBox = styled.div`
+    padding: 0.5rem;
+    margin: 0.5rem;
+    margin: 10px;
+    text-align: center;
+    display: flex;
+    flex-wrap: wrap;
+    .gifs, .stickers {
+        background: olive; 
+    }
+`
+
+const Button = styled.button`
+    cursor: pointer;
+    background: black;
+    color: white;
+    width: 6rem;
+    height: 2rem;
+    /* &:hover{
+        background: olive;
+    } */
+`
+export default SearchPage;
